70fa01648a586788275ed7f096dc6073da03ab46
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2024-12-17 08:19:38 -0800

    add logo

--- sankey_dashboard/src/routes/+layout.svelte
+++ sankey_dashboard/src/routes/+layout.svelte
@@ -11,18 +11,20 @@
 	import Icon from "@iconify/svelte";
 </script>
 
-<div class="flex flex-row w-screen font-sans h-[120px] pl-8  bg-gradient-to-r from-[#2B7F9E] to-[#5e1f04] items-center gap-8 text-white text-3xl background-image url('/banner.png')">	
+<div class="flex flex-row w-screen font-sans h-[120px] pl-8  bg-gradient-to-r from-[#2B7F9E] to-[#5e1f04] items-center gap-8 text-white text-3xl">	
 
         <a href="{base}/">Water-Energy Sankey Diagrams for the United States</a>
+		<img src="PNNLVerticalLogo.png" alt="card-image" class='h-[100px] ml-auto pr-7'/>
 
 </div>
 
-<div class="flex flex-row w-screen font-sans h-[60px] pl-8 bg-none items-center gap-8 text-gray-800 rounded-s text-gray-700 border-2">
+<div class="flex flex-row w-screen font-sans h-auto pl-8 bg-none items-center gap-8 text-gray-800 rounded-s text-gray-700 border-2">
     
     {#each page_items as item}
         <a href="{base}/{item === "diagrams" ? '' : item}" class="hover:text-gray-500">{item.toUpperCase()}</a>
     {/each}
 </div>
+
 
 <div class="flex flex-row w-screen h-[20px] pl-8  bg-white gap-8">	
 
