8d56944c6b87f7990ccaa0261ac4e7078366c018
Kendall Mongird <74064300+kmongird@users.noreply.github.com>
2024-12-17 11:10:05 -0800

    Update +page.svelte

--- sankey_dashboard/src/routes/+page.svelte
+++ sankey_dashboard/src/routes/+page.svelte
@@ -238,7 +238,7 @@
       Scenario Selection
     </h5>
   </div>
-  <nav class="flex min-w-[240px] flex-col gap-3 font-sans text-base font-normal text-blue-gray-700">
+  <nav class="flex min-w-[240px] flex-col gap-3 mb-8 font-sans text-base font-normal text-blue-gray-700">
     <div role="contentinfo"
       class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
       
@@ -356,7 +356,7 @@
         height: 800px;
         /* overflow-x: hidden;
         overflow-y: auto; */
-        position: static;
+        /* position: static; */
         /* display: flex; */
         flex-direction: column;
         align-items: center;
