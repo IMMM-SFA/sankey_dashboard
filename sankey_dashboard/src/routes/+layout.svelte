c513589fb4c13bbb40b3b56c522680be8768ef3b
hniazi <hassan.niazi@pnnl.gov>
2025-01-21 03:17:05 -0500

    add alpha version disclaimer

--- sankey_dashboard/src/routes/+layout.svelte
+++ sankey_dashboard/src/routes/+layout.svelte
@@ -15,6 +15,10 @@
 <div class="flex flex-row w-screen font-sans h-[120px] pl-8  bg-gradient-to-r from-[#2B7F9E] to-[#5e1f04] items-center gap-8 text-white text-3xl">	
 
 	<a href="{base}/">Integrated Energy-Water Flows for the United States over the 21st Century</a>
+    <div class="flex flex-col items-center ml-auto pr-4">
+        <div class="bg-red-500 text-white text-sm font-semibold px-2 py-1 text-center rounded">Alpha Version</div>
+        <div class="text-white text-xs mt-1">For internal use only. Do not share</div>
+    </div>
 	<img src="PNNLVerticalLogo.png" alt="card-image" class='h-[100px] ml-auto pr-7'/>
 
 </div>
